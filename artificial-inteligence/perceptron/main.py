0a5bb9cff9a202f7fef590d416a86df59bd5fee6
paulopieczarka <paulopieczarka@gmail.com>
2019-05-30 14:22:39 -0300

    Add pycache to ignore  impl train_test_split

--- artificial-inteligence/perceptron/main.py
+++ artificial-inteligence/perceptron/main.py
@@ -1,8 +1,56 @@
 from votes_dataset import HouseVotes_DataSet
+from sklearn.model_selection import train_test_split
 import numpy as np
 
 # open and preprocess house-votes dataset
-x, y, dataset = HouseVotes_DataSet()
+xs, ys, dataset = HouseVotes_DataSet()
 
-print(x)
-print(y)
+class Perceptron:
+  def __init__ (self):
+    self.weights = None
+    self.threshold = None
+
+  def model (self, xs):
+    return 1 if (np.dot(self.weights, x) >= self.threshold) else 0
+
+  def predict (self, xs):
+    y = [self.model(x) for x in xs]
+    return np.array(y)
+
+  def fit (self, xs, ys, epochs = 1, lr = 1):
+    self.weights = np.ones(xs.shape[1])
+    self.threshold = 0
+
+    accuracy = {}
+    max_accuracy = 0
+    wt_matrix = []
+
+    # for all epochs
+    for i in range(epochs):
+      for x, y in zip(xs, ys):
+        y_pred = self.model(x)
+        if (y == 1 and y_pred == 0):
+          self.weights = self.weights + lr * x
+          self.threshold = self.threshold - lr * 1
+        elif (y == 0 and y_pred == 1):
+          self.weights = self.weights - lr * x
+          self.threshold = self.threshold + lr * 1
+
+      wt_matrix.append(self.weights)
+      accuracy[i] = accuracy_score(self.predict(xs), ys)
+      if (accuracy[i] > max_accuracy):
+        max_accuracy = accuracy[i]
+        chkptw = self.weights
+        chkptb = self.threshold
+
+    # checkpoint -> save the weights and threshold value
+    self.weights = chkptw
+    self.threshold = chkptb
+
+    print(max_accuracy)
+
+# train test split
+x_train, x_test, y_train, y_test = train_test_split(xs, ys, test_size = 0.1, stratify = ys, random_state = 1)
+
+perceptron = Perceptron()
+# mat = perceptron.predict(xs)
